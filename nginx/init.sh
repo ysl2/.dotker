cebd33f98881ad40dff77286025395fb594f5660
ysl2 <www.songli.yu@gmail.com>
2023-03-09 03:22:35 +0800

    1

---
+++ nginx/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo docker run \
+    -d \
+    -p 80:80 \
+    -v /home/yusongli/Documents/mybook:/www/mybook \
+    -v /home/yusongli/.dotker/nginx/etc/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf \
+    -v /home/yusongli/.dotker/nginx/etc/nginx/htpasswd:/etc/nginx/htpasswd \
+    -v /home/yusongli/.dotker/nginx/etc/nginx/nginx.conf:/etc/nginx/nginx.conf \
+    --name nginx \
+    nginx
+
+# Add new htpasswd config:
+# htpasswd -bc ./htpasswd user1 *******
+
+# Update htpasswd config:
+# htpasswd -b ./htpasswd user2 *******
