37b647a384613abd2ff728e111a2cde1795e6042
ysl2 <www.songli.yu@gmail.com>
2023-03-10 11:43:31 +0800

    1

--- nginx/init.sh
+++ nginx/init.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # NOTE: You only need to modify www project target. Don't modify port.
-# 
+#
 # Hints: htpasswd
+# Don't use `-b` option, otherwise nginx won't update htpasswd config.
 #
 # ```
 # # Add new htpasswd config:
-# htpasswd -bc ./htpasswd user1 *******
+# htpasswd -c ./htpasswd user1
 #
 # # Update htpasswd config:
-# htpasswd -b ./htpasswd user2 *******
+# htpasswd ./htpasswd user2
 # ```
 #
 # (Optional) If you want to deploy docsify into nginx docker:
@@ -24,7 +25,7 @@
 #    ```
 #    # Exec this when you are outside container:
 #    sudo docker exec -it nginx /bin/bash
-#    
+#
 #    # After into container:
 #    ln -s /bin/lib-nodejs/bin/node /bin/node
 #    cd /www
